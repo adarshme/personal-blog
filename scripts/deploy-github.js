c25e5868178b63db2a3fb7789a1c1e8adc9c7e41
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2020-05-12 14:34:46 +0530

    Added deploy script

---
+++ scripts/deploy-github.js
@@ -0,0 +1,13 @@
+const ghpages = require("gh-pages")
+
+// replace with your repo url
+ghpages.publish(
+  "public",
+  {
+    branch: "master",
+    repo: "https://github.com/InfernoCoder11/blog.git",
+  },
+  () => {
+    console.log("Deploy Complete!")
+  }
+)
