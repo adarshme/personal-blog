2d03acd073b0f4af524ba36b531918f6af4d1efa
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2020-05-16 18:59:36 +0530

    Added ignore option in purgcss
    It was removing a lot of required markdown styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/blog`,
+  // pathPrefix: `/blog`,
   siteMetadata: {
     title: `Inferno's Blog`,
     siteUrl: `https://infernocoder11.github.io/blog`,
@@ -50,8 +50,8 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         //develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        //whitelist: ["pre"], // Don't remove this selector
+        ignore: ["markdown.css"], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
     },
