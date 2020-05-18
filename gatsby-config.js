add252f54e57e88a1269cbf8acffe4b54e117211
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2020-05-18 13:51:16 +0530

    Whitelisted gatsby-focus-wrapper in purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         //develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        //whitelist: ["pre"], // Don't remove this selector
+        whitelist: ["gatsby-focus-wrapper"], // Don't remove this selector
         ignore: ["markdown.css"], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
