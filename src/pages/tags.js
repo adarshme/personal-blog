52681502169e6c7fbf3c49fd649b625e5b7214f3
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2020-05-19 15:46:40 +0530

    Added basic tags page

---
+++ src/pages/tags.js
@@ -0,0 +1,67 @@
+import React from "react"
+import PropTypes from "prop-types"
+// Utilities
+// import kebabCase from "lodash/kebabCase"
+// Components
+import SEO from "../components/seo"
+import { Link, graphql } from "gatsby"
+import Header from "../components/header"
+import Footer from "../components/footer"
+const TagsPage = ({
+  data: {
+    allMdx: { group },
+    site: {
+      siteMetadata: { title },
+    },
+  },
+}) => (
+  <div>
+    <SEO title="Tags" />
+    <div>
+      <Header siteTitle={title} />
+      <div className="tags-container">
+        <ul className="tags-list">
+          {group.map(tag => (
+            <li key={tag.fieldValue} className="tag">
+              <Link to={`/tags/${tag.fieldValue}/`}>{tag.fieldValue}</Link>
+            </li>
+          ))}
+        </ul>
+        <Footer />
+      </div>
+    </div>
+  </div>
+)
+TagsPage.propTypes = {
+  data: PropTypes.shape({
+    allMdx: PropTypes.shape({
+      group: PropTypes.arrayOf(
+        PropTypes.shape({
+          fieldValue: PropTypes.string.isRequired,
+          totalCount: PropTypes.number.isRequired,
+        }).isRequired
+      ),
+    }),
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+      }),
+    }),
+  }),
+}
+export default TagsPage
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMdx(limit: 2000) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`
